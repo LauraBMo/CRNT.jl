cdb361d9b0e314ee219263848e18b4fd530b8c36
Laura Brustenga i Moncusí <brust@math.ku.dk>
2020-07-06 01:23:49 +0200

    add dependencies
    Add Nemo, Polymake, LinearAlgebra

--- src/CRNT.jl
+++ src/CRNT.jl
@@ -2,4 +2,8 @@ module CRNT
 
 # Write your package code here.
 
+# We need Nemo for the kernel of N1
+import Nemo # matrix, FlintIntegerRing, nullspace
+import Polymake # polytope.cone,intersection
+import LinearAlgebra: I, dot
 end
