c78087ae1145caa600c50417f6e609ee29b6d889
Laura Brustenga i Moncusí <brust@math.ku.dk>
2020-07-06 16:51:40 +0200

    add tests for NonnegativeNullspaceCone

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using CRNT
 using Test
 
 @testset "CRNT.jl" begin
-    # Write your tests here.
+    @test NonnegativeNullspaceCone([0 0; 0 0]) == [1 0; 0 1]
+    @test NonnegativeNullspaceCone([1 -1; 2 -2]) == transpose([1 1])
 end
