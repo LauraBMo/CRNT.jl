5e671fc46116569eae0c7b487bda77ef636ee93c
Laura Brustenga i Moncusí <brust@math.ku.dk>
2020-11-08 16:51:24 +0100

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,20 @@ using CRNT
 using Test
 using Nemo
 
-@testset "CRNT.jl" begin
-    @test NonnegativeNullspaceCone([0 0; 0 0]) == [1 0; 0 1]
-    @test NonnegativeNullspaceCone([1 -1; 2 -2]) == transpose([1 1])
+@testset "Cones.jl" begin
+    @test cone_positivenullspace([0 0; 0 0]) == [1 0; 0 1]
+    @test cone_positivenullspace([1 -1; 2 -2]) == transpose([1 1])
 end
 
-@testset "TypesCompatibilites.jl" begin
+@testset "Base.jl" begin
     a = 0.999
     @test Float16(Nemo.fmpq(a)) == Float16(a)
-    @test Nemo.fmpq(a) == 999//1000
+    @test Nemo.fmpq(a) == 999 // 1000
     a = 0.9999999
     @test Float32(Nemo.fmpq(a)) == Float32(a)
     a = 0.99999999
     @test Float64(Nemo.fmpq(a)) == Float64(a)
     a = 0.11111111111111111111111
     @test_broken BigFloat(Nemo.fmpq(a)) == BigFloat(a)
-    @test push!([0.1], Nemo.fmpq(1//2)) == [0.1; 0.5]
+    @test push!([0.1], Nemo.fmpq(1 // 2)) == [0.1; 0.5]
 end
